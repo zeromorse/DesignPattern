97631a4d243f284dcc0aff8b445969fe91258f83
ZeroMorse <1042564554@qq.com>
2017-05-24 22:04:08 +0800

    为不变模式添加样例

--- src/main/java/N16_immutable/test/Demo.java
+++ src/main/java/N16_immutable/test/Demo.java
@@ -1,4 +1,12 @@
 package N16_immutable.test;
 
+import N16_immutable.code.Fraction;
+
 public class Demo {
+    public static void main(String[] args) {
+        Fraction fraction1 = new Fraction(1, 4);
+        Fraction fraction2 = fraction1.multiply(new Fraction(2, 1));
+        System.out.println(fraction1);
+        System.out.println(fraction2);
+    }
 }
